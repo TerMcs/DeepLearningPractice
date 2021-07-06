d3e3eb7ff46c51385c9c21790f3ff0d5010680c2
TerMcs <ter.mcs@gmail.com>
2021-07-06 12:09:26 +0300

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='eagnach_chasta',
+    version='0.1',
+    description='Deep learning trial package',
+    py_modules=["train", "utils"],
+    install_requires=['numpy']
+)
+# test
