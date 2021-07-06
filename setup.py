cd71c5250e86fd2f63be7c8e76510bc4336ecc80
TerMcs <ter.mcs@gmail.com>
2021-07-06 16:02:59 +0300

    update

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup(
     version='0.1',
     description='Deep learning trial package',
     py_modules=["train", "utils"],
-    install_requires=['torch', 'torchvision', 'numpy', 'hydra']
+    install_requires=['torch', 'torchvision', 'numpy', 'hydra'],
+    # entry_points = {
+    #     '...': [
+    #         'entry point function' = ...
+    #     ]
+    # },
 )
-# test
+
