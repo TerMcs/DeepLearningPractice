d48fd190d6d3dc7a56fa59c5e5648bb53ee972b2
TerMcs <ter.mcs@gmail.com>
2021-07-08 08:53:32 +0300

    Prevent dataloader from re-downloading data each time (hydra cwd issue).

--- utils.py
+++ utils.py
@@ -9,13 +9,13 @@ from torchvision import datasets
 #def my_transforms():
 
 def get_mnist(batch_size):
-    train_dataset = datasets.MNIST(root='data',
+    train_dataset = datasets.MNIST(root='../../data',
                                    train=True,
                                    transform=transforms.ToTensor(),
                                    download=True
                                    )
 
-    test_dataset = datasets.MNIST(root='data',
+    test_dataset = datasets.MNIST(root='../../data',
                                   train=False,
                                   transform=transforms.ToTensor(),
                                   download=True
